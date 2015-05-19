3706e00a4133cf5ce1205b33a712af8ce2b5dba4
Alexandre <alexandre.mangin@viacesi.fr>
2015-05-19 10:00:48 +0200

    Add french stopwords in map reduce function

--- app/Services/MapReduce.php
+++ app/Services/MapReduce.php
@@ -97,7 +97,19 @@ class MapReduce
                 "your", "yours", "yourself", "yourselves", "the","didnt", "doesnt", "dont",
                 "isnt", "wasnt", "youre", "hes", "ive", "theyll", "whos", "wheres", "whens",
                 "whys", "hows", "whats", "were", "shes", "im", "thats", "just","did","This",
-                "this","come","did","amp;",";","&amp;"];
+                "this","come","did","amp;",";","&amp;",
+
+                "alors","au","aucuns","aussi","autre","avant","avec","avoir","bon","car","ce","cela",
+                "ces","ceux","chaque","ci","comme","comment","dans","des","du","dedans","dehors","depuis",
+                "devrait","doit","donc","dos","début","elle","elles","en","encore","essai","est","et","eu",
+                "fait","faites","fois","font","hors","ici","il","ils","je","juste","la","le","les","leur",
+                "là","ma","maintenant","mais","mes","mine","moins","mon","mot","même","ni","nommés","notre",
+                "nous","ou","où","par","parce","pas","peut","peu","plupart","pour","pourquoi","quand","que",
+                "quel","quelle","quelles","quels","qui","sa","sans","ses","seulement","si","sien","son","sont",
+                "sous","soyez","sujet","sur","ta","tandis","tellement","tels","tes","ton","tous","tout","trop",
+                "très","tu","voient","vont","votre","vous","vu","ça","étaient","état","étions","été","être"
+
+                ];
 
                 var patt = new RegExp("[a-zA-Z]{3,}");
 
